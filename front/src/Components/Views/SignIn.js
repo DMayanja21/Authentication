ecfc6da1a1b27d387bc235eb1a1911550dca220d
DMayanja21 <you@example.com>
2019-11-20 12:34:42 +0300

    update

---
+++ front/src/Components/Views/SignIn.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export default class SignIn extends Component {
+    
+//     state = {
+//         firstName:"First Name",
+//         lastName:"Last Name",
+//         password:"Password"    
+//     }
+    
+    
+//     render() {
+//         return (
+//             <div>
+//                 <form>
+//                     <input type="text" name="firstName" required placeholder="First Name"></input>
+//                     <input type="text" name="firstName" required placeholder="First Name"></input>
+//                     <input type="text" name="firstName" required placeholder="First Name"></input>
+//                 </form>
+
+//                 <form>
+//                 <input type="text" name="firstName" required placeholder="First Name"></input>
+//                 <input type="text" name="firstName" required placeholder="First Name"></input>
+//                 <input type="text" name="firstName" required placeholder="First Name"></input>
+//                 <input type="text" name="firstName" required placeholder="First Name"></input>
+                
+                
+//                 </form>
+//             </div>
+//         )
+//     }
+// }
